4dfec209197fa8d8b3c76cf602d9cf2fad8d154c
azam <azamhonkhodjaev@gmail.com>
2023-08-30 12:51:15 +0500

    first

---
+++ java.js
@@ -0,0 +1,15 @@
+
+    let ab = +prompt('Введите число')
+    let ba = +prompt('Введите степень')
+if (ab == 0 || ba === 0) {
+    alert( '0 ')
+    
+}else if( isNaN(ab) && (ba) || isNaN(ba) && (ab) ) {
+    alert('Ошибка')
+}else if((ab > 0) || (ba > 0)) {
+    alert (ab  + ' в '  + ba  + ' степени = ' + (ab ** ba))
+}else if( isNaN(ab) || isNaN(ba)) {
+    alert(' Ошибка ')
+}else if( isNaN(ab) || isNaN(ba)) {
+    alert (' Ошибка ')
+}
